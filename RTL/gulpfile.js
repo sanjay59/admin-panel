381dc33c27aec504462f153469ca90e661b083bf
sanjay59 <sanjayyadav224210@gmail.com>
2022-02-06 23:24:29 +0530

    Admin Panel

---
+++ RTL/gulpfile.js
@@ -0,0 +1,177 @@
+var gulp = require('gulp');
+sass = require("gulp-sass"),
+postcss = require("gulp-postcss");
+autoprefixer = require("autoprefixer");
+var sourcemaps = require('gulp-sourcemaps'); 
+var browserSync = require('browser-sync').create(); 
+var cssbeautify = require('gulp-cssbeautify');
+var beautify = require('gulp-beautify');
+
+
+/*******************  LTR   ******************/
+
+
+//_______ task for scss folder to css main style 
+gulp.task('watch-rtl', function() {
+	console.log('Command executed successfully compiling scss in assets.');
+	 return gulp.src('Templist/assets/scss-rtl/**/*.scss') 
+		.pipe(sourcemaps.init())                       
+		.pipe(sass())
+		.pipe(sourcemaps.write(''))   
+		.pipe(gulp.dest('Templist/assets/css-rtl'))
+		.pipe(browserSync.reload({
+		  stream: true
+	}))
+})
+
+
+//_______task for admin-custom
+gulp.task('admin-rtl', function(){
+   return gulp.src('Templist/assets/css-rtl/admin-custom.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass())
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('Templist/assets/css-rtl'));
+		
+})
+
+
+//_______task for sidemenu
+gulp.task('menu-rtl', function(){
+   return gulp.src('Templist/assets/css-rtl/sidemenu.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass())
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('Templist/assets/css-rtl'));
+		
+})
+
+//_______task for skin-modes
+gulp.task('skins-rtl', function(){
+   return gulp.src('Templist/assets/css-rtl/skin-modes.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass())
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('Templist/assets/css-rtl'));
+		
+})
+/*******************  Color-skins   ******************/
+
+
+//_______task for color1
+gulp.task('color1-rtl', function(){
+   return gulp.src('Templist/assets/color-skins/color1.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass())
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('Templist/assets/color-skins'));
+		
+})
+
+//_______task for color2
+gulp.task('color2-rtl', function(){
+   return gulp.src('Templist/assets/color-skins/color2.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass())
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('Templist/assets/color-skins'));
+		
+})
+
+//_______task for color3
+gulp.task('color3-rtl', function(){
+   return gulp.src('Templist/assets/color-skins/color3.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass())
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('Templist/assets/color-skins'));
+		
+})
+
+//_______task for color4
+gulp.task('color4-rtl', function(){
+   return gulp.src('Templist/assets/color-skins/color4.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass())
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('Templist/assets/color-skins'));
+		
+})
+
+
+//_______task for color5
+gulp.task('color5-rtl', function(){
+   return gulp.src('Templist/assets/color-skins/color5.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass())
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('Templist/assets/color-skins'));
+		
+})
+
+//_______task for color6
+gulp.task('color6-rtl', function(){
+   return gulp.src('Templist/assets/color-skins/color6.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass())
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('Templist/assets/color-skins'));
+		
+})
+
+//_______task for color7
+gulp.task('color7-rtl', function(){
+   return gulp.src('Templist/assets/color-skins/color7.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass())
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('Templist/assets/color-skins'));
+		
+})
+
+//_______task for color8
+gulp.task('color8-rtl', function(){
+   return gulp.src('Templist/assets/color-skins/color8.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass())
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('Templist/assets/color-skins'));
+		
+})
+
+//_______task for color9
+gulp.task('color9-rtl', function(){
+   return gulp.src('Templist/assets/color-skins/color9.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass())
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('Templist/assets/color-skins'));
+		
+})
+
+//_______task for color10
+gulp.task('color10-rtl', function(){
+   return gulp.src('Templist/assets/color-skins/color10.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass())
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('Templist/assets/color-skins'));
+		
+})
+
+/*******************  Beautify  ******************/
+
+//_______ task for beautifying css
+gulp.task('beautify-rtl', function() {
+    return gulp.src('Templist/assets/css-rtl/*.css')
+        .pipe(beautify.css({ indent_size: 4 }))
+        .pipe(gulp.dest('Templist/assets/css-rtl'));
+});
+
+
+//_______ task for beautifying colorskins
+gulp.task('beautifycolor-rtl', function() {
+    return gulp.src('Templist/assets/color-skins/*.css')
+        .pipe(beautify.css({ indent_size: 4 }))
+        .pipe(gulp.dest('Templist/assets/color-skins'));
+});
